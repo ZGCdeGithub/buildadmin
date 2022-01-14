7d9ee02ee06efd7149f39be67a797a5b2b74b057
妙码生花 <18523774412@qq.com>
2022-01-14 23:32:04 +0800

    refactor(i18n):element-plus国际化支持

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-    <router-view></router-view>
+    <el-config-provider :locale="lang">
+        <router-view></router-view>
+    </el-config-provider>
 </template>
 <script setup lang="ts">
 import { onMounted, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
 import iconfontInit from '/@/utils/iconfont'
 import { useRoute } from 'vue-router'
 import { setTitle } from '/@/utils/common'
+import { store } from '/@/store/index'
+import { Language } from 'element-plus/lib/locale'
 
 const route = useRoute()
+
+// 初始化 element 的语言包
+const { getLocaleMessage } = useI18n()
+const lang = getLocaleMessage(store.getters['config/getStateOrCache']('defaultLang')) as Language
 
 onMounted(() => {
     iconfontInit()
